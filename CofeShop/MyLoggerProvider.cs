207e85b4befd635755abd3554e97dc53629a9ca8
Rozzini <Rozzinimr@gmail.com>
2020-01-19 00:20:26 +0800

    add logger

---
+++ CofeShop/MyLoggerProvider.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace CofeShop
+{
+    internal class MyLoggerProvider : ILoggerProvider
+    {
+        public ILogger CreateLogger(string categoryName)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
