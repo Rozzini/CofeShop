eb68983aedde893648d143791eff09ad562202ee
Rozzini <Rozzinimr@gmail.com>
2020-01-19 09:24:24 +0800

    added db test methods in program.cs

--- CofeShop/Program.cs
+++ CofeShop/Program.cs
@@ -17,6 +17,8 @@ namespace CofeShop
 
         public static void Main(string[] args)
         {
+            int choice = 2;
+
             var builder = new ConfigurationBuilder();
             
             builder.SetBasePath(Directory.GetCurrentDirectory());
@@ -31,16 +33,84 @@ namespace CofeShop
             var options = optionsBuilder
                 .UseSqlServer(connectionString)
                 .Options;
-
-            using (ApplicationContext db = new ApplicationContext(options))
+            do
             {
-                var goods = db.Goods.ToList();
-                foreach (Good g in goods)
+                Console.WriteLine("Type 1: to show DB;\n     2: add data to BD;\n     3: delete data from DB;\n     0: exit");
+
+                choice = Convert.ToInt32(Console.ReadLine());
+
+                switch (choice)
                 {
-                    Console.WriteLine($"{g.Id}.{g.Name} - {g.Price} - {g.Currency} - {g.Quantity} - {g.Category}.");
+                    
+                    case 1:
+                        using (ApplicationContext db = new ApplicationContext(options))
+                        {
+                            var goods = db.Goods.ToList();
+                            foreach (Good g in goods)
+                            {
+                                Console.WriteLine($"{g.Id}.{g.Name} - {g.Price} - {g.Currency} - {g.Quantity} - {g.Category}.");
+                            }
+                        }
+                        Console.WriteLine("input any button to continue");
+                        Console.ReadLine();
+                        Console.Clear();
+                        break;
+                        
+                    case 2:
+                        string name;
+                        double price;
+                        string currency;
+                        uint quantity;
+                        string category;
+                        Console.WriteLine("Name : " + (name = Convert.ToString(Console.ReadLine())) + 
+                                          "Price : " + (price = Convert.ToDouble(Console.ReadLine())) + 
+                                          "Currency : " + (currency = Convert.ToString(Console.ReadLine())) + 
+                                          "Quantity : " + (quantity = Convert.ToUInt32(Console.ReadLine())) +
+                                          "Categpty : " + (category = Convert.ToString(Console.ReadLine()))
+                                          );
+
+                        using (ApplicationContext db = new ApplicationContext(options))
+                        {
+                            Good good = new Good { Name = name, Price = price, Currency = currency, Quantity = quantity, Category = category };
+                            db.Goods.Add(good);
+                            db.SaveChanges();
+                        }
+                        Console.WriteLine("input any button to continue");
+                        Console.ReadLine();
+                        Console.Clear();
+                        break;
+
+                    case 3:
+                        int id;
+                        Console.WriteLine("input id: ");
+                        id = Convert.ToInt32(Console.ReadLine());
+                        using (ApplicationContext db = new ApplicationContext(options))
+                        { 
+                            Good good = new Good { Id = id };
+                            db.Goods.Remove(good);
+                            db.SaveChanges();
+                        }
+                        Console.WriteLine("input any button to continue");
+                        Console.ReadLine();
+                        Console.Clear();
+                        break;
+
+                    case 0:
+                        Console.WriteLine("Good night son\n");
+                        Console.WriteLine("input any button to continue");
+                        Console.ReadLine();
+                        Console.Clear();
+                        break;
+
+                    default:
+                        Console.WriteLine("Incorrect data input\n");
+                        Console.WriteLine("input any button to continue");
+                        Console.ReadLine();
+                        Console.Clear();
+                        break;
                 }
-            }
-            Console.Read();
+                
+            } while (choice != 0);
         }
     }
 }
